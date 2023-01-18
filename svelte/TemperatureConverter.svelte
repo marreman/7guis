724759ddc572cf1bb1f84fc297abac5c939fc66b
Martin Liberg <martin.liberg@hey.com>
2023-01-18 22:38:09 +0100

    Create TemperatureConverter.svelte

---
+++ svelte/TemperatureConverter.svelte
@@ -0,0 +1,21 @@
+<script>
+  let celsius = null;
+  let fahrenheit = null;
+
+  function toFahrenheit() {
+    fahrenheit = celsius ? celsius * (9 / 5) + 32 : null
+  }
+  
+  function toCelsius() {
+    celsius = fahrenheit != null ? (fahrenheit - 32) * (5 / 9) : null
+  }
+</script>
+
+<input type="number" bind:value={celsius} on:input={toFahrenheit} /> Celsius =
+<input type="number" bind:value={fahrenheit} on:input={toCelsius} /> Fahrenheit
+
+<style>
+  input {
+    width: 100px;
+  }
+</style>
